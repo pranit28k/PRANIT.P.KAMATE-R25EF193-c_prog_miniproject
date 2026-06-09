6764bbff0b5777f7ccf6410bc839ec37d0153359
pranit28k <pranitkamate281007@gmail.com>
2026-06-09 23:01:45 +0530

    update2

--- miniproject.c
+++ miniproject.c
@@ -65,3 +65,69 @@ int objectCount = 0;
     int sy = (y1 < y2) ? 1 : -1;
 
     int err = dx - dy;
+while (1) {
+        setPixel(x1, y1, ch);
+
+        if (x1 == x2 && y1 == y2)
+            break;
+
+        int e2 = 2 * err;
+
+        if (e2 > -dy) {
+            err -= dy;
+            x1 += sx;
+        }
+
+        if (e2 < dx) {
+            err += dx;
+            y1 += sy;
+        }
+    }
+}
+
+void drawRectangle(int x1, int y1,
+                   int x2, int y2,
+                   char ch) {
+    drawLine(x1, y1, x2, y1, ch);
+    drawLine(x2, y1, x2, y2, ch);
+    drawLine(x2, y2, x1, y2, ch);
+    drawLine(x1, y2, x1, y1, ch);
+}
+
+void drawCircle(int xc, int yc,
+                int r, char ch) {
+    int x = 0;
+    int y = r;
+    int d = 3 - 2 * r;
+
+    while (y >= x) {
+
+        setPixel(xc + x, yc + y, ch);
+        setPixel(xc - x, yc + y, ch);
+        setPixel(xc + x, yc - y, ch);
+        setPixel(xc - x, yc - y, ch);
+
+        setPixel(xc + y, yc + x, ch);
+        setPixel(xc - y, yc + x, ch);
+        setPixel(xc + y, yc - x, ch);
+        setPixel(xc - y, yc - x, ch);
+
+        x++;
+
+        if (d > 0) {
+            y--;
+            d += 4 * (x - y) + 10;
+        } else {
+            d += 4 * x + 6;
+        }
+    }
+}
+
+void drawTriangle(int x1, int y1,
+                  int x2, int y2,
+                  int x3, int y3,
+                  char ch) {
+    drawLine(x1, y1, x2, y2, ch);
+    drawLine(x2, y2, x3, y3, ch);
+    drawLine(x3, y3, x1, y1, ch);
+}
